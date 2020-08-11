0086d3839fbf73d49e586034b00539052e18f826
drewparker <drewjparker91@gmail.com>
2020-08-11 13:31:20 -0700

    Add js, jquery, and intitial clickable functions
    Co-authored-by William DS <wdstwo@live.com>

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $(.button).click(function(){
+    
+    
+  }
+  $(".clickable").click(function() {
+    $("#walrus-showing").wrap();
+    $("#walrus-hidden").wrap();
+  });
+  $(".clickable1").click(function() {
+    $("#aardvark-showing").wrap();
+    $("#aardvark-hidden").wrap();
+  });
+  $(".clickable2").click(function() {
+    $("#aardvark-showing").wrap();
+    $("#aardvark-hidden").wrap();
+  });
+  $(".clickable3").click(function() {
+    $("#aardvark-showing").wrap();
+    $("#aardvark-hidden").wrap();
+  });
+});
